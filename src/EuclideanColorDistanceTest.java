253a8b07f3706cf97b89a4f398f063642528cd10
Kimberly Mageary <kimberlymageary@gmail.com>
2025-04-15 15:37:41 -0700

    Adding that initial test file for wave 3

---
+++ src/EuclideanColorDistanceTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+
+
+public class EuclideanColorDistanceTest {
+    
+}
