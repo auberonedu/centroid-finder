1459d36cb371cd5b1005260d24f77c35450b53d3
Kimberly Mageary <kimberlymageary@gmail.com>
2025-04-17 12:02:38 -0700

    Set up intial testing environment

---
+++ src/BinarizingImageGroupFinderTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class BinarizingImageGroupFinderTest {
+    
+}
