408874302cc91ec433b55729e1d71f78f19d3c7f
Alexander Ruban <alexruban29@gmail.com>
2025-04-18 12:51:38 -0700

    AI Used: Added more tests for helper method to test more colors.

--- src/EuclideanColorDistanceTest.java
+++ src/EuclideanColorDistanceTest.java
@@ -9,4 +9,28 @@ public class EuclideanColorDistanceTest {
         EuclideanColorDistance distance = new EuclideanColorDistance();
         assertArrayEquals(new int[] {0, 0, 0}, distance.convertHexToRGB(0x000000));
     }
+
+    @Test
+    public void testConvertHexToRGB_White() {
+        EuclideanColorDistance distance = new EuclideanColorDistance();
+        assertArrayEquals(new int[] {255, 255, 255}, distance.convertHexToRGB(0xFFFFFF));
+    }
+
+    @Test
+    public void testConvertHexToRGB_Yellow() {
+        EuclideanColorDistance distance = new EuclideanColorDistance();
+        assertArrayEquals(new int[] {255, 255, 0}, distance.convertHexToRGB(0xFFFF00));
+    }
+
+    @Test
+    public void testConvertHexToRGB_Blue() {
+        EuclideanColorDistance distance = new EuclideanColorDistance();
+        assertArrayEquals(new int[] {0, 0, 255}, distance.convertHexToRGB(0x0000FF));
+    }
+
+    @Test
+    public void testConvertHexToRGB_RandomColor() {
+        EuclideanColorDistance distance = new EuclideanColorDistance();
+        assertArrayEquals(new int[] {18, 52, 86}, distance.convertHexToRGB(0x123456));
+    }
 }
