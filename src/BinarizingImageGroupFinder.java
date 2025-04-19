a43eac531fd28a8ad62632d1be2521c2d950d2f7
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-04-19 08:44:10 -0700

    Added calls to binarizer and groupfinder in BinarizingImageGroupFinder.java to return groups in our image

--- src/BinarizingImageGroupFinder.java
+++ src/BinarizingImageGroupFinder.java
@@ -37,6 +37,7 @@ public class BinarizingImageGroupFinder implements ImageGroupFinder {
      */
     @Override
     public List<Group> findConnectedGroups(BufferedImage image) {
-        return null;
+        int[][] binaryImage = binarizer.toBinaryArray(image);
+        return groupFinder.findConnectedGroups(binaryImage);
     }
 }
