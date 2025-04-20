a9e395126d98c6c797d9ef5cbef1cfbb443146be
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-04-20 06:10:25 -0700

    Wrote EuclidianColorDistance based on pseudocode, separating colors into individual color values for easier comparison.

--- src/EuclideanColorDistance.java
+++ src/EuclideanColorDistance.java
@@ -19,6 +19,26 @@ public class EuclideanColorDistance implements ColorDistanceFinder {
      */
     @Override
     public double distance(int colorA, int colorB) {
-        return 0;
+        // red data is stored in bits 23-16
+        // green data is stored in bits 15-8
+        // blue data is stored in bits 7-0
+
+        int r1 = (colorA >> 16) & 0xFF; // shifts bits 23-16 down to 7-0, masks remaining
+        int g1 = (colorA >> 8) & 0xFF; // shifts bits 15-8 down to 7-0, masks remaining
+        int b1 = colorA & 0xFF; // no shift is required since bits are already 7-0, masks remaining
+
+        // same process for the second color
+        int r2 = (colorB >> 16) & 0xFF;
+        int g2 = (colorB >> 8) & 0xFF;
+        int b2 = colorB & 0xFF;
+
+        // distance variables created to be used in the formula for simplicity
+        int dr = r1 - r2; // red distance
+        int dg = g1 - g2; // green distance
+        int db = b1 - b2; // blue distance
+
+        // sqrt((r1 - r2)^2 + (g1 - g2)^2 + (b1 - b2)^2) original formula
+        // sqrt((dr)^2 + (dg)^2 + (db)^2) new formula with distance variables
+        return Math.sqrt(dr * dr + dg * dg + db * db);
     }
 }
