b45a95bc9ed489bb353fc485d1d6f3e6355744dd
Kimberly Mageary <kimberlymageary@gmail.com>
2025-04-21 10:05:43 -0700

    Added some new test cases

--- src/BinarizingImageGroupFinderTest.java
+++ src/BinarizingImageGroupFinderTest.java
@@ -1,8 +1,10 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.*;
 import java.awt.image.BufferedImage;
 import org.junit.Test;
 
@@ -37,5 +39,46 @@ public class BinarizingImageGroupFinderTest {
         public List<Group> findConnectedGroups(int[][] image){
             return predefinedGroups;
         }
+    }
+
+    @Test
+    public void testReturnsPredefinedGroups(){
+        BufferedImage dummyImage = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
+        int[][] fakeBinary = {
+            {1, 0}, 
+            {1, 0}
+        };
+
+        Group group1 = new Group(2, new Coordinate(1, 0));
+        List<Group> predefinedGroups = List.of(group1);
+
+        BinarizingImageGroupFinder groupFinder = new BinarizingImageGroupFinder(new FakeBinarizer(fakeBinary), 
+        new FakeGroupFinder(predefinedGroups));
+
+        List<Group> result = groupFinder.findConnectedGroups(dummyImage);
+
+        assertEquals(predefinedGroups, result);
+    }
+
+    @Test 
+    public void testNoWhitePixels(){
+        BufferedImage dummyImage = new BufferedImage(3, 3, BufferedImage.TYPE_INT_RGB);
+        int[][] fakeBinary = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        };
+
+        List<Group> emptyGroups = List.of();
+        BinarizingImageGroupFinder groupFinder = new BinarizingImageGroupFinder(new FakeBinarizer(fakeBinary), 
+        new FakeGroupFinder(emptyGroups));
+
+        List<Group> result = groupFinder.findConnectedGroups(dummyImage);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testMultipleGroupsDescOrder(){
+        BufferedImage dummyImage = new BufferedImage(3, 3, BufferedImage.TYPE_INT_RGB);
     }
 }
