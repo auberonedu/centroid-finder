23b5327236daeab71a506464b24cf53a820c818f
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-04-21 15:01:06 -0700

    started to create test for binarizing image group finder

---
+++ src/BinarizingImageGroupFinderTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+import interfaces.ColorDistanceFinder;
+
+public class BinarizingImageGroupFinderTest {
+
+    @Test
+    public void test_test() {
+
+        // Define test colors
+        int colorA = 0x000000;
+        int colorB = 0xFFFFFF;
+
+    
+        // Create a color distance finder
+        ColorDistanceFinder colorDistance = new EuclideanColorDistance(colorA, colorB);
+        // CREATE A BINARIZER (input: color distance finder, int color, int threshold)
+        DistanceImageBinarizer binarizer = new DistanceImageBinarizer(, 0, 0);
+        // Create group finder
+        DfsBinaryGroupFinder finder = new DfsBinaryGroupFinder();
+
+        // CREATE AN IMAGE GROUP FINDER (input: image binarizer, binary group finder)
+        BinarizingImageGroupFinder testImageGroupFinder = new BinarizingImageGroupFinder(null, finder);
+
+
+        /*    public BinarizingImageGroupFinder(ImageBinarizer binarizer, BinaryGroupFinder groupFinder) {
+        this.binarizer = binarizer;
+        this.groupFinder = groupFinder;
+    } */
+
+    }
+    
+}
