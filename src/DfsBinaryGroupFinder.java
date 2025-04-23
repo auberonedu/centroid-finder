b5058cb3b39222eea04aedbf85908621162a15c2
Danny McCarragher <dannymccarragher@gmail.com>
2025-04-23 16:09:16 -0700

    Correctly implemented logic to sort in descending order. Reverses compareTo.

--- src/DfsBinaryGroupFinder.java
+++ src/DfsBinaryGroupFinder.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DfsBinaryGroupFinder implements BinaryGroupFinder {
    /**
     * Finds connected pixel groups of 1s in an integer array representing a binary image.
-    * 
+    *
     * The input is a non-empty rectangular 2D array containing only 1s and 0s.
     * If the array or any of its subarrays are null, a NullPointerException
     * is thrown. If the array is otherwise invalid, an IllegalArgumentException
@@ -15,7 +16,7 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
     * (x:0, y:0). Y increases downward and X increases to the right. For example,
     * (row:4, column:7) corresponds to (x:7, y:4).
     *
-    * The method returns a list of sorted groups. The group's size is the number 
+    * The method returns a list of sorted groups. The group's size is the number
     * of pixels in the group. The centroid of the group
     * is computed as the average of each of the pixel locations across each dimension.
     * For example, the x coordinate of the centroid is the sum of all the x
@@ -25,10 +26,10 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
     * The division should be done as INTEGER DIVISION.
     *
     * The groups are sorted in DESCENDING order according to Group's compareTo method
-    * (size first, then x, then y). That is, the largest group will be first, the 
-    * smallest group will be last, and ties will be broken first by descending 
+    * (size first, then x, then y). That is, the largest group will be first, the
+    * smallest group will be last, and ties will be broken first by descending
     * y value, then descending x value.
-    * 
+    *
     * @param image a rectangular 2D array containing only 1s and 0s
     * @return the found groups of connected pixels in descending order
     */
@@ -39,7 +40,7 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
 
         int expectedWidth = image[0].length;
         List<Group> coordinates = new ArrayList<>();
-        
+
         // search entire image looking for connections of 1's
         for(int row = 0; row < image.length; row++){
             if (image[row] == null || image[row].length != expectedWidth) {
@@ -57,8 +58,11 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
                     int ySum = 0;
 
                     for(int[] cord : coordinate){
-                        xSum += cord[1];
-                        ySum += cord[0];
+                        int y = cord[0];
+                        int x = cord[1];
+
+                        xSum += x;
+                        ySum += y;
                     }
 
                     //x = sum of all x coords in group / size
@@ -69,6 +73,8 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
             }
         }
 
+        //sort coordinates in descending order (reverses compareTo method)
+        coordinates.sort(Collections.reverseOrder());
         return coordinates;
     }
 
@@ -96,5 +102,5 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
         }
 
     }
-    
+
 }
