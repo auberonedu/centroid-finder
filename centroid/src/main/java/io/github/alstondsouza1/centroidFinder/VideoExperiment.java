43ff09760eb31d6ae636febcf884e1bbe787c9f1
alstondsouza1 <alstondsouza35@gmail.com>
2025-05-11 09:06:37 -0700

    tested the library and it worked well!

--- centroid/src/main/java/io/github/alstondsouza1/centroidFinder/VideoExperiment.java
+++ centroid/src/main/java/io/github/alstondsouza1/centroidFinder/VideoExperiment.java
@@ -2,6 +2,7 @@ package io.github.alstondsouza1.centroidFinder;
 
 import org.jcodec.api.FrameGrab;
 import org.jcodec.api.JCodecException;
+import org.jcodec.common.io.NIOUtils;
 import org.jcodec.common.io.SeekableByteChannel;
 import org.jcodec.common.model.Picture;
 import org.jcodec.scale.AWTUtil;
@@ -9,24 +10,20 @@ import org.jcodec.scale.AWTUtil;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 
 public class VideoExperiment {
     public static void main(String[] args) {
-        File videoFile = new File("sampleInput/sample.mp4");
+        File videoFile = new File("sampleInput/sample.mp4"); // Relative path
 
-        try (FileInputStream fis = new FileInputStream(videoFile);
-             FileChannel channel = fis.getChannel()) {
-
-            FrameGrab grab = FrameGrab.createFrameGrab((SeekableByteChannel) channel);
+        try (SeekableByteChannel ch = NIOUtils.readableChannel(videoFile)) {
+            FrameGrab grab = FrameGrab.createFrameGrab(ch);
 
             int frameNumber = 0;
             Picture picture;
             while ((picture = grab.getNativeFrame()) != null && frameNumber < 5) {
                 BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
-                File output = new File("frame_" + frameNumber + ".png");
+                File output = new File("sampleOutput/frame_" + frameNumber + ".png");
                 ImageIO.write(bufferedImage, "png", output);
                 System.out.println("Saved frame " + frameNumber);
                 frameNumber++;
