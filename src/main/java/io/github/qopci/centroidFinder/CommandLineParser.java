fe9df8e7597c0dcc66efcb61b01f32b18d67c893
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-05-14 14:09:13 -0700

    Added variables for the command-line

--- src/main/java/io/github/qopci/centroidFinder/CommandLineParser.java
+++ src/main/java/io/github/qopci/centroidFinder/CommandLineParser.java
@@ -1,5 +1,9 @@
 package io.github.qopci.centroidFinder;
 
 public class CommandLineParser {
-    
+    // Variables to store parsed command-line arguments
+    public final String inputPath;
+    public final String outputCsv;
+    public final int targetColor;
+    public final int threshold;
 }
