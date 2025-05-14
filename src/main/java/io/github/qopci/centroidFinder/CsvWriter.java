600e89719127be38a7947bc5cb9907bfb5485573
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-05-14 14:45:49 -0700

    Added pseudo comments and plan for CsvWriter

--- src/main/java/io/github/qopci/centroidFinder/CsvWriter.java
+++ src/main/java/io/github/qopci/centroidFinder/CsvWriter.java
@@ -6,4 +6,8 @@ import java.io.IOException;
 
 public class CsvWriter {
     
+    //private field for writer
+    //constructor here: open the output file and write the headerline
+    //write the coordinate pair to CSV file
+    //close the file when done writing
 }
