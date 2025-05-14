8e55c5897c1e79891cde54125fba7448b0e1f1c0
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-05-14 14:50:03 -0700

    Added CSVWriter method that throws IOException

--- src/main/java/io/github/qopci/centroidFinder/CsvWriter.java
+++ src/main/java/io/github/qopci/centroidFinder/CsvWriter.java
@@ -1,13 +1,19 @@
 package io.github.qopci.centroidFinder;
 
 import java.io.PrintWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class CsvWriter {
-    
-    //private field for writer
+    private final PrintWriter writer;
+
     //constructor here: open the output file and write the headerline
+    public CsvWriter(String path) throws IOException {
+        this.writer = new PrintWriter(new FileWriter(path)); 
+        this.writer.println("x,y"); //this writes the CSV header
+    }
     //write the coordinate pair to CSV file
     //close the file when done writing
+
 }
