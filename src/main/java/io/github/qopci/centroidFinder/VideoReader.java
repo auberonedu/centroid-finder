1a0e02e50545fd82783a5084a447889488a92eac
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-05-14 14:22:15 -0700

    Added private variables into VideoReader file for JavaCV usage

--- src/main/java/io/github/qopci/centroidFinder/VideoReader.java
+++ src/main/java/io/github/qopci/centroidFinder/VideoReader.java
@@ -4,5 +4,8 @@ import org.bytedeco.javacv.*;
 import java.awt.image.BufferedImage;
 
 public class VideoReader {
-    
+    private final FFmpegFrameGrabber grabber;
+    private final Java2DFrameConverter converter = new Java2DFrameConverter();
+    private double frameRate;
+    private int frameNumber = 0;
 }
