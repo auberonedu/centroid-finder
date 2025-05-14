9071f796ae17d4df6c6dd8f0165f021c79d79e22
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-05-14 14:55:39 -0700

    Cleaned up the comments in CsvWriter.java, and removed an extra import

--- src/main/java/io/github/qopci/centroidFinder/CsvWriter.java
+++ src/main/java/io/github/qopci/centroidFinder/CsvWriter.java
@@ -1,26 +1,25 @@
 package io.github.qopci.centroidFinder;
 
 import java.io.PrintWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class CsvWriter {
     private final PrintWriter writer;
 
-    //constructor here: open the output file and write the headerline
+    // Constructor here: open the output file and write the headerline
     public CsvWriter(String path) throws IOException {
         this.writer = new PrintWriter(new FileWriter(path)); 
-        this.writer.println("x,y"); //this writes the CSV header
+        this.writer.println("x,y"); // Writes the CSV header
     }
-    //write the coordinate pair to CSV file
+    
+    // Write the coordinate pair to CSV file
     public void write(Coordinate c) {
         writer.printf("%d,%d%n", c.x(), c.y()); // Write x and y values as a CSV line
     }
 
-    //close the file when done writing
+    // Close the file when done writing
     public void close() {
-        writer.close(); // Ensure file is properly closed
+        writer.close(); 
     }
-
 }
