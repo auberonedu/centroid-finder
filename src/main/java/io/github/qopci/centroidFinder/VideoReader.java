26839076053b8feea631ed547b6377ed06ff6933
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-05-14 14:36:24 -0700

    Cleaned up the comments spacing

--- src/main/java/io/github/qopci/centroidFinder/VideoReader.java
+++ src/main/java/io/github/qopci/centroidFinder/VideoReader.java
@@ -9,14 +9,14 @@ public class VideoReader {
     private double frameRate;
     private int frameNumber = 0;
 
-    //Constructor here: this is the video grabber, where it starts
+    // Constructor here: this is the video grabber, where it starts
     public VideoReader(String path) throws Exception {
         grabber = new FFmpegFrameGrabber(path); // Use JavaCV to open the video file
         grabber.start(); 
         frameRate = grabber.getFrameRate(); 
     }
 
-    //Read the next frame from vid, then return the bufferedImage
+    // Read the next frame from vid, then return the bufferedImage
     public BufferedImage nextFrame() throws Exception {
         Frame frame = grabber.grabImage(); 
         frameNumber++;
@@ -28,13 +28,13 @@ public class VideoReader {
         }
     }
 
-    //then check whether the current frame should processed as new SECOND
+    // Check whether the current frame should processed as new SECOND
     public boolean shouldProcessThisFrame() {
         // Only process one fps — if frameNumber crosses a second boundary
         return (int)((frameNumber - 1) / frameRate) != (int)(frameNumber / frameRate);
     }
     
-    //Close the video grabber 
+    // Close the video grabber 
     public void close() throws Exception {
         grabber.close();
     }
