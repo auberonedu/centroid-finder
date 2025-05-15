ad5af7d650d554d03eb72e4852a0e8b690ab504b
alstondsouza1 <alstondsouza35@gmail.com>
2025-05-15 14:22:27 -0700

    added FrameProcessor to return largest centroid

--- centroid/src/main/java/io/github/alstondsouza1/centroidFinder/FrameProcessor.java
+++ centroid/src/main/java/io/github/alstondsouza1/centroidFinder/FrameProcessor.java
@@ -1,5 +1,21 @@
 package io.github.alstondsouza1.centroidFinder;
 
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+// processes a single video to find the largest centroid
 public class FrameProcessor {
+    private final ImageGroupFinder finder;
     
+    // constructor
+    public FrameProcessor(ImageGroupFinder finder) {
+        this.finder = finder;
+    }
+
+    // returns cen
+    public Coordinate getLargestCentroid(BufferedImage frame) {
+        List<Group> groups = finder.findConnectedGroups(frame);
+        if (groups == null || groups.isEmpty()) return new Coordinate(-1, -1);
+        return groups.get(0).centroid(); // largest group
+    }
 }
