8f5784857b6c82c9cc4175b465b413880675a472
Danny McCarragher <dannymccarragher@gmail.com>
2025-05-16 12:22:34 -0700

    Start of implementing Image Processor

--- src/main/java/io/github/jameson789/app/ImageProcessor.java
+++ src/main/java/io/github/jameson789/app/ImageProcessor.java
@@ -1,5 +1,25 @@
 package io.github.jameson789.app;
 
-public class ImageProcessor {
+import java.awt.image.BufferedImage;
+import java.util.List;
 
+public class ImageProcessor {
+    private final ImageBinarizer binarizer;
+    private final ImageGroupFinder groupFinder;
+
+    public ImageProcessor(int targetColor, int threshold) {
+        ColorDistanceFinder distanceFinder = new EuclideanColorDistance();
+        this.binarizer = new DistanceImageBinarizer(distanceFinder, targetColor, threshold);
+        this.groupFinder = new BinarizingImageGroupFinder(binarizer, new DfsBinaryGroupFinder());
+    }
+
+    public CentroidResult processImage(BufferedImage image) {
+        List<Group> groups = groupFinder.findConnectedGroups(image);
+        Group largest = groups.stream()
+                .max((a, b) -> Integer.compare(a.size(), b.size()))
+                .orElse(null);
+        return (largest != null)
+                ? new CentroidResult(largest.centroid().x(), largest.centroid().y())
+                : null;
+    }
 }
