21ac7149bb254d413323d2bff5de89f1be1d3ae1
= <hellohi12465@gmail.com>
2025-05-17 23:43:04 -0700

    Done with FrameProcessorTest. Will modify the corresponding java files to allow for the addition of more testcases easy, on top of simple testcases.

--- centroid/src/test/java/io/github/alstondsouza1/centroidFinder/FrameProcessorTest.java
+++ centroid/src/test/java/io/github/alstondsouza1/centroidFinder/FrameProcessorTest.java
@@ -1,5 +1,43 @@
 package io.github.alstondsouza1.centroidFinder;
 
+import org.junit.jupiter.api.Test;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FrameProcessorTest {
-    
+
+    class MockImageGroupFinder implements ImageGroupFinder {
+        @Override
+        public List<Group> findConnectedGroups(BufferedImage image) {
+            return List.of(new Group(5, new Coordinate(7, 9)));
+        }
+    }
+
+    class EmptyImageGroupFinder implements ImageGroupFinder {
+        @Override
+        public List<Group> findConnectedGroups(BufferedImage image) {
+            return List.of();
+        }
+    }
+
+    @Test
+    public void testLargestCentroidReturnedCorrectly() {
+        FrameProcessor processor = new FrameProcessor(new MockImageGroupFinder());
+        BufferedImage dummy = new BufferedImage(10, 10, BufferedImage.TYPE_INT_RGB);
+
+        Coordinate c = processor.getLargestCentroid(dummy);
+        assertEquals(new Coordinate(7, 9), c);
+    }
+
+    @Test
+    public void testNoGroupsReturnsNegativeOne() {
+        FrameProcessor processor = new FrameProcessor(new EmptyImageGroupFinder());
+        BufferedImage dummy = new BufferedImage(10, 10, BufferedImage.TYPE_INT_RGB);
+
+        Coordinate c = processor.getLargestCentroid(dummy);
+        assertEquals(new Coordinate(-1, -1), c);
+    }
 }
