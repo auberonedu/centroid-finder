53ce5ee065975bad47964088c2d5763cdd20318c
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-05-19 14:21:07 -0700

    Updated Video processorApp to take account for time

--- src/main/java/io/github/qopci/centroidFinder/VideoProcessorApp.java
+++ src/main/java/io/github/qopci/centroidFinder/VideoProcessorApp.java
@@ -3,29 +3,33 @@ package io.github.qopci.centroidFinder;
 import java.awt.image.BufferedImage;
 
 public class VideoProcessorApp {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             CommandLineParser parser = new CommandLineParser(args);
             FrameProcessor processor = new FrameProcessor(parser.targetColor, parser.threshold);
 
-            // Open video reader and CSV writer
-            try (
-                VideoReader reader = new VideoReader(parser.inputPath);
-                CsvWriter writer = new CsvWriter(parser.outputCsv)
-            ) {
+            try (VideoReader reader = new VideoReader(parser.inputPath);
+                 CsvWriter writer = new CsvWriter(parser.outputCsv)) {
+
+                // Hardcoded frame rate, ensantina has a frame rate of 23.98
+                // But we'll still with 24 fps
+                double frameRate = 24;  
+                int frameCount = 0;
+
                 BufferedImage frame;
-                // Process each frame
                 while ((frame = reader.nextFrame()) != null) {
                     if (reader.shouldProcessThisFrame()) {
                         Coordinate centroid = processor.process(frame);
-                        writer.write(centroid); // Write centroid to CSV
+                        double timeInSeconds = frameCount / frameRate;
+                        writer.write(timeInSeconds, centroid);
+                        frameCount++;
                     }
                 }
             }
 
             System.out.println("Done. CSV written to " + parser.outputCsv);
         } catch (Exception e) {
-            e.printStackTrace(); // Print errors if any
+            e.printStackTrace();
         }
     }
 }
