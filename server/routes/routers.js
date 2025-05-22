f2a26ee5a3ea690c275308e7c8ce1604cbc9648c
f3liz <felixchen14@gmail.com>
2025-05-22 12:35:13 -0700

    created routes

--- server/routes/routers.js
+++ server/routes/routers.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const router = express.Router();
+
+router.get("/api/videos",);
+router.get("/thumbnail/:filename",);
+router.post("/process/:filename",);
+router.get("/process/:jobId/status",);
+
+export default router;
