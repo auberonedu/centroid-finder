5eb66a3331c8ecf82ed2b4e91c7aa9a9d7c36623
Danny McCarragher <dannymccarragher@gmail.com>
2025-05-21 21:45:19 -0700

    Method for getting first frame of video using ffmpeg

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,12 +1,13 @@
 import fs from 'fs/promises';
 import path from 'path';
+import ffmpeg from 'fluent-ffmpeg';
 
 const getVideos = async (req, res) => {
   try {
     const videoDir = path.join(process.cwd(), 'videos');
     const files = await fs.readdir(videoDir);
 
-    const videoURLs = files.map(file => `/videos/${file}`);
+    const videoURLs = files.map(file => `${file}`);
     res.json(videoURLs);
   } catch (err) {
     console.error('Error reading video directory:', err);
@@ -14,6 +15,30 @@ const getVideos = async (req, res) => {
   }
 };
 
+const getThumbnail = (req, res) => {
+  try {
+
+    const videoDir = path.join(process.cwd(), 'videos');
+    const thumbnailId = req.params.filename;
+    const videoPath = path.join(videoDir, thumbnailId);
+
+    res.setHeader('Content-Type', 'image/jpeg');
+
+    ffmpeg(videoPath)
+      .inputOptions(['-ss 0'])        // jump to start
+      .outputOptions(['-frames:v 1']) // capture 1 frame
+      .format('mjpeg')                // output format as JPEG
+      .pipe(res, { end: true });      // stream directly to response
+  } catch (err) {
+    console.error('Error reading video directory:', err);
+    res.status(500).json({
+      error: "Error generating thumbnail"
+    });
+  };
+
+}
+
 export default {
-    getVideos
+  getVideos,
+  getThumbnail
 };
