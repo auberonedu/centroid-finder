47eea7e88a13a717b0153d623e17235c41283f00
Danny McCarragher <dannymccarragher@gmail.com>
2025-05-21 22:07:49 -0700

    Configure dotenv for filepath

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,9 +2,16 @@ import fs from 'fs/promises';
 import path from 'path';
 import ffmpeg from 'fluent-ffmpeg';
 
+// this is built in into node so no need to install 
+import { randomUUID } from 'crypto';
+
 const getVideos = async (req, res) => {
+
+  console.log(process.env.VIDEO_PATH)
+
   try {
-    const videoDir = path.join(process.cwd(), 'videos');
+    // const videoDir = path.join(process.cwd(), 'videos');
+    const videoDir = process.env.VIDEO_PATH;
     const files = await fs.readdir(videoDir);
 
     const videoURLs = files.map(file => `${file}`);
@@ -18,7 +25,7 @@ const getVideos = async (req, res) => {
 const getThumbnail = (req, res) => {
   try {
 
-    const videoDir = path.join(process.cwd(), 'videos');
+    const videoDir = process.env.VIDEO_PATH;
     const thumbnailId = req.params.filename;
     const videoPath = path.join(videoDir, thumbnailId);
 
@@ -34,6 +41,32 @@ const getThumbnail = (req, res) => {
     res.status(500).json({
       error: "Error generating thumbnail"
     });
+  };
+
+  const startProcess = (req, res) => {
+    const { targetColor, threshold } = req.query;
+
+    if (!targetColor || !threshold) {
+      return res.status(400).json({ error: "Missing targetColor or threshold query parameter." })
+    }
+    try {
+      const videoDir = process.env.VIDEO_PATH;
+
+
+      const jobId = randomUUID();
+
+      // TODO: Start async job (e.g. queue system or background worker)
+      console.log(`Starting processing job for ${filename}`);
+      console.log(`Target Color: ${targetColor}, Threshold: ${threshold}`);
+      console.log(`Job ID: ${jobId}`);
+
+      // Respond to client with 202 Accepted and jobId
+      res.status(202).json({ jobId });
+
+    } catch (err) {
+      console.error('Error starting job:', err);
+      res.status(500).json({ error: 'Error starting job' });
+    }
   };
 
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import router from './router/router.js';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 const app = express();
 const PORT = 3000;
