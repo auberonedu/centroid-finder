c90628488d6a7494eeac41ddf167ab249104bf1b
Alexander Ruban <alexruban29@gmail.com>
2025-05-23 12:40:09 -0700

    Added checks for no file name and if file was found.

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -1,6 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import ffmpeg from 'fluent-ffmpeg';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,9 +23,23 @@ const getAllVideos = async (req, res) => {
   }
 };
 
-const getVideoThumbnail = (req, res) => {
+const getVideoThumbnail = async (req, res) => {
   try {
-    
+    const { filename } = req.params;
+
+    // Checking if no file name was passed
+    if (!filename) {
+      return res.status(400).json({ error: "Filename is required"})
+    }
+
+    const videoPath = path.join(VIDEO_DIR, filename);
+
+    // Checking if the file was found
+    try {
+      await fs.access(videoPath);
+    } catch {
+      return res.status(404).json({ error: "Video file not found" });
+    }
 
   } catch (err) {
     console.error("Error generating thumbnail", err);
