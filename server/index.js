6cbc121c43ba46f238ec1f73780eebb4797e6d89
f3liz <felixchen14@gmail.com>
2025-05-23 11:51:55 -0700

    working on the route to get all videos

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import router from "./routes/routers.js"
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(express.json()); // read in JSON payloads in request body
+// read in JSON payloads in request body
+app.use(express.json());
+
+// serve static files from /videos
+app.use('/videos', express.static(path.resolve('public/videos')));
 
 app.use("/", router);
 
-app.listen(3000, console.log("Listening on http://localhost:3000"));
+app.listen(PORT, console.log("Listening on http://localhost:3000"));
