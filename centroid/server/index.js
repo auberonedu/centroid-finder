3261643412d6af066beed3ac0214ed1dc4b9db1b
alstondsouza1 <alstondsouza35@gmail.com>
2025-05-22 22:39:47 -0700

    started with basic setup express and connected route modules

--- centroid/server/index.js
+++ centroid/server/index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const videoRoutes = require('./routes/videoRoutes');
+const processRoutes = require('./routes/processRoutes');
+
+app.use(express.json());
+
+app.use('/api/videos', videoRoutes);
+app.use('/process', processRoutes);
+
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Salamander Tracker Server Running');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
