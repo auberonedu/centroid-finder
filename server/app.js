93fe24df019d802e9cfbdd8644aabeebe2f503fe
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-05-23 12:02:19 -0700

    Added code to use express.static in order to serve the files from videos and results folders

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import router from './routes/serverApi.js'; 
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
 
+app.use('/videos', express.static(path.resolve('./videos')));
+app.use('/results', express.static(path.resolve('./results')));
 app.use('/', router);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
