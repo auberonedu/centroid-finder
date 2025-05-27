96e0245c64fbf7d3cf3d742901c6b6c7eb99d96b
Stefanurkal <stephenfranada@gmail.com>
2025-05-26 18:29:50 -0700

    progress

--- server/Controller/Controller.js
+++ server/Controller/Controller.js
@@ -1,11 +1,19 @@
 import fs from 'fs';
 import path from 'path';
+
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+
+import ffmpeg from 'fluent-ffmpeg';
+import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 // Required in ES modules to get __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
+
 const rawVideoPath = process.env.VIDEO_DIR 
 // Go up one directory from /server/Controller to /project/videos
 
@@ -29,11 +37,24 @@ const getVideos = async (req, res) => {
 };
 
 // const getPreview = async(req,res) => {
+// ffmpeg(videoPath)
+//   .on('end', () => {
+//     res.sendFile(outputImagePath);
+//   })
+//   .on('error', (err) => {
+//     console.error('Error extracting frame:', err);
+//     res.status(500).json({ error: 'Failed to extract frame' });
+//   })
+//   .screenshots({
+//     timestamps: ['00:00:01.000'],       // 1 second in
+//     filename: 'frame.jpg',
+//     folder: path.join(__dirname, '../frames')
 
+//   });
 // } 
 
 // const startVideoProcessing = async(req,res) => {
-
+// this will call the jar from maven 
 // }
 
 // const getJobStatus = async(req, res) => {
