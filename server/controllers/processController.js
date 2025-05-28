412bd89142fa5c69c1376e9038979a680a8ca94d
f3liz <felixchen14@gmail.com>
2025-05-27 22:41:00 -0700

    controller to start video processing job completed

--- server/controllers/processController.js
+++ server/controllers/processController.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import fs from 'fs';
 import dotenv from 'dotenv';
+import { spawn } from 'child_process';
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
@@ -20,6 +22,29 @@ const startVideoProcessingJob = (req, res) => {
 
     const inputPath = path.join(VIDEO_DIR, fileName);
     const outputPath = path.join(OUTPUT_DIR);
+    const jobId = uuidv4();
+
+    const args = [
+        '-jar',
+        JAR_PATH,
+        inputPath,
+        targetColor,
+        threshold,
+        outputPath
+    ];
+
+    try {
+        const child = spawn('java', args, {
+            detached: true,
+            stdio: 'ignore'
+        });
+
+        child.unref();
+        return res.status(202).json({ jobId });
+    } catch (err) {
+        console.error("Failed to start processing job: ", err);
+        return res.status(500).json({ error: "Error starting job"});
+    }
 };
 
 const getProcessingJobStatus = (req, res) => {
