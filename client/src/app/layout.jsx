a80ae711a7d911447acec350a5c5fcf7ffb0e940
Stefanurkal <stephenfranada@gmail.com>
2025-06-01 16:17:25 -0700

    fixed the layout.jsx  will need to style it

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,19 +1,21 @@
 import Link from  "next/link";
 //import styles from './Layout.module.css';
 
-export default function Layout({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/videos">Videos</Link></li>
-        </ul>
-      </nav>
-
-      <main>
-        {children}
-      </main>
-    </div>
+    <html lang="en">
+      <body>
+        <div>
+          <nav className="navbar">
+            <ul>
+              <li><Link href="/">Home</Link></li>
+              <li><Link href="/videos">Videos</Link></li>
+              <li><Link href="/videos/status">Completed</Link></li>
+            </ul>
+          </nav>
+          <main>{children}</main>
+        </div>
+      </body>
+    </html>
   );
 }
