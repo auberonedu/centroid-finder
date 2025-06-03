7325479e3c850b9c1efa64313b0180e35bdf7a47
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-06-03 10:22:46 -0700

    Installed cors for backend connect to the frontend

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import router from './routes/serverApi.js'; 
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
+
+app.use(cors());
 
 app.use('/videos', express.static(path.resolve('../videos')));
 app.use('/results', express.static(path.resolve('../results')));
