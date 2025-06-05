864b5be023f8096ff14b98147d6301704e60e659
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-06-05 09:25:59 -0700

    Updating backend to work with frontend

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import route from "./routers/router.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/", route);
 app.use("/results", express.static(path.resolve(process.env.OUTPUT_DIR)));
 app.use("/videos", express.static(path.resolve(process.env.VIDEO_DIR)));
