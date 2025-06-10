76f63c90f2e82bd905ea12528215934075f9c2d0
Stefanurkal <stephenfranada@gmail.com>
2025-06-09 19:31:51 -0700

    added favicon and image to nav bar

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -1,11 +1,16 @@
 "use client";
 import Link from "next/link";
+import Image from 'next/image';
 import { AppBar, Toolbar, Button, Box } from "@mui/material";
 
 export default function NavBar() {
   return (
     <AppBar position="static">
+
       <Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
+        <Box sx={{ display: "flex", alignItems: "left", gap: 1 }}>
+          <Image src="/logo.png" alt="Logo" width={75} height={50} />
+        </Box>
         <Button color="inherit" component={Link} href="/videos">
           Video List
         </Button>
