b55458015c8682f605929395a70d102b07dd569e
Stefanurkal <stephenfranada@gmail.com>
2025-06-11 20:53:01 -0700

    made small change to nav bar

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -7,15 +7,12 @@ export default function NavBar() {
   return (
     <AppBar position="static">
 
-      <Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Box sx={{ display: "flex", alignItems: "left", gap: 1 }}>
           <Image src="/logo.png" alt="Logo" width={75} height={50} />
         </Box>
         <Button color="inherit" component={Link} href="/videos">
           Video List
-        </Button>
-        <Button color="inherit" component={Link} href="/preview">
-          Preview Page
         </Button>
         <Button color="inherit" component={Link} href="/results/example-job-id">
           Results (Demo)
